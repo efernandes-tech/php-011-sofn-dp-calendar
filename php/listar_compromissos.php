11e06e3ffd1e44227f903e14c42e217e4e225ef9
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-07-25 21:52:03 -0300

    exibindo dados

---
+++ php/listar_compromissos.php
@@ -0,0 +1,23 @@
+<?php
+
+header('Content-Type: application/json');
+
+require 'conexao.php';
+
+$sql = "SELECT * FROM agendamentos";
+
+$query = $conn->query($sql);
+
+$rows = $query->fetchAll(PDO::FETCH_ASSOC);
+
+$resultado = array();
+
+foreach($rows as $i => $row) {
+	$resultado[$i]['id'] = (int) $row['id'];
+	$resultado[$i]['title'] = $row['nome'];
+	$resultado[$i]['start'] = date("Y-m-d\TH:i:s", strtotime($row['inicio']));
+	$resultado[$i]['end'] = date("Y-m-d\TH:i:s", strtotime($row['fim']));
+}
+
+echo json_encode($resultado);
+exit;
